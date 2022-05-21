bbeb511b5843963ff9060e6f2305bfe4d2508025
Johnlin2922 <johnlin2922@gmail.com>
2022-05-21 15:15:27 -0500

    update keyboard width

--- src/contexts/BoardContext.jsx
+++ src/contexts/BoardContext.jsx
@@ -19,7 +19,7 @@ const BoardContextProvider = (props) => {
     const [currentRow, setCurrentRow] = useState(0);
     const [currentPosition, setCurrentPosition] = useState(0);
     let status = "";
-    const word = "FLOYD";
+    const word = "PINK FLOYD";
 
     const handleSubmit = () => {
         let row = boardState[currentRow];
